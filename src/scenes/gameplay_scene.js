af77fc6e8829a23343e36e93ebe33e0e471aea4b
kiwon132445 <kiwon132445@gmail.com>
2021-02-20 14:57:28 -0800

    covid functionality

--- src/scenes/gameplay_scene.js
+++ src/scenes/gameplay_scene.js
@@ -11,6 +11,7 @@ class gameplay_scene extends Phaser.Scene {
 
     preload() {
         this.load.image('player', '../../assets/dog.jpg');
+        this.load.image('covid', '../../assets/covid.jpg')
     }
 
     create() {
@@ -18,11 +19,12 @@ class gameplay_scene extends Phaser.Scene {
     }
     
     update() {
+        //the player control
         this.player.player_controls();
     }
 
+    //This adds the player to the game
     addPlayer() {
-        console.log(this.scale.width)
         this.player = new Player(
           {
             scene: this,
@@ -30,11 +32,16 @@ class gameplay_scene extends Phaser.Scene {
             y: this.scale.height/2,
             sprite: 'player',
           },
-          300,
+          200,
           this.covid
         );
     
-        this.add.existing(this.player).setScale(0.1);
+        this.add.existing(this.player).setScale(0.01);
         this.physics.add.existing(this.player);
-      }
+        this.player.setCollideWorldBounds(true);
+    }
+
+    spawnCovid() {
+
+    }
 }
