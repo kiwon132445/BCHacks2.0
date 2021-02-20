e544f687cc18cc2451e1213acb029261dcc20082
johnlim612 <johnlim0612@gmail.com>
2021-02-20 14:33:06 -0800

    Update title_screen_scene.js

--- src/scenes/title_screen_scene.js
+++ src/scenes/title_screen_scene.js
@@ -15,6 +15,7 @@ class title_screen_scene extends Phaser.Scene {
         const screenCenterX = this.cameras.main.worldView.x + screenX / 2;
         const screenCenterY = this.cameras.main.worldView.y + screenY / 2;
 
+<<<<<<< Updated upstream
         this.startButton = new TextButton(
             this,
             screenCenterX,
@@ -22,8 +23,30 @@ class title_screen_scene extends Phaser.Scene {
             'play',
             { font: '40px Ariel', fill: 'blue' },
             () => this.scene.start('gameplay_scene'));
+=======
+        this.playButton = this.add.text(screenCenterX - 45, screenCenterY, 'Play', {font: '45px Ariel', fill: 'white'})
+            .setInteractive()
+            .on('pointerover', () => this.buttonHoverState())
+            .on('pointerout', () => this.buttonRestState())
+            .on('pointerdown', () => this.buttonActiveState())
+            .on('pointerup', () => {
+                this.scene.start('gameplay_scene')
+            })
+>>>>>>> Stashed changes
     }
 
+    buttonHoverState() {
+        this.playButton.setStyle({ fill: '#5DADE2'});
+      }
+    
+    buttonRestState() {
+        this.playButton.setStyle({ fill: 'white'});
+      }
+    
+    buttonActiveState() {
+        this.playButton.setStyle({ fill: 'blue' });
+    }
+        
     update(){
     }
 }
