a1121b36201dcd262b6632d634afe48294b6d89a
kiwon132445 <kiwon132445@gmail.com>
2021-02-20 15:31:12 -0800

    server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+app.use(express.static(__dirname + '/src'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/src/scenes", express.static(__dirname + '/src/scenes'));
+app.use("/src", express.static(__dirname + '/src'));
+app.use("/assets", express.static(__dirname + '/assets'));
+
+let port = process.env.PORT || 8000;
+
+server.listen(port, function () {
+    console.log(`Listening on ${server.address().port}`);
+  });
