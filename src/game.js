c8efc98224f64d66066f0e52d79114e19498943d
kmoh-byte <54565124+kmoh-byte@users.noreply.github.com>
2021-02-20 22:34:36 -0800

    Changes to css and javascript

--- src/game.js
+++ src/game.js
@@ -21,3 +21,12 @@ var config = {
 };
 
 const game = new Phaser.Game(config);
+
+window.addEventListener("keyup", ev => {
+  if (ev.keyCode === 38) {
+    //YOUR CODE HERE
+    
+    //example below
+    document.body.innerHTML = "<h1 id='gameOn'>GAME ON!!!</h1>"
+  }
+});
